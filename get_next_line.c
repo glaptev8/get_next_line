755132f09f551fd011c2361da703f78ed5bcae88
gleblaptev <gleb.laptev.00@bk.ru>
2019-09-22 21:15:21 +0300

    "everthing is ok but maybe memory"

--- get_next_line.c
+++ get_next_line.c
@@ -2,69 +2,42 @@
 #include "get_next_line.h"
 #include "libft/libft.h"
 
-
-static void	*new_str(char **str, int fd)
+int		new_str(char **str, int fd, char **line)
 {
-	size_t	len;
-	int		i;
-	char	*new_str;
-	i = 0;
+	int		len;
+	char	*tmp;
+
 	len = 0;
-	while (str[fd][len] != '\n' && str[fd][len] != '\0')
+	while (str[fd][len] != '\0' && str[fd][len] != '\n')
 		len++;
-	new_str = ft_strsub(str[fd], 0, len);
-//	if (str[fd][len] == '\n')
-//		*str += len + 1;
-//	else if (str[fd][len] == '\0')
-//		*str += len;
-while (len-- > 0)
-	str[fd]++;
-str[fd]++;
-return (new_str);
+	*line = ft_strsub(str[fd], 0, len);
+	tmp = ft_strdup(str[fd] + len + (str[fd][len] == '\n' ? 1 : 0));
+	free(str[fd]);
+	str[fd] = tmp;
+	return (1);
 }
-int		get_next_line(const int fd, char **line)
 
+int get_next_line(const int fd, char **line)
 {
-	int 			ret;
-	char			buf[BUFF_SIZE + 1];
-	static	char	*s[512];
+	static char	*str[252];
+	char 		*tmp;
+	int 		ret;
+	char 		buf[BUFF_SIZE + 1];
 
-	if (fd == -1 || !line)
+	if (fd < 0 || !line)
 		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)))
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		if (!s[fd])
-			s[fd] = ft_strnew(ret);
-//		free(s[fd]);
-		s[fd] = ft_strjoin(s[fd], buf);
+		if (!str[fd])
+			str[fd] = ft_strnew(1);
+		tmp = ft_strjoin(str[fd], buf);
+		free(str[fd]);
+		str[fd] = tmp;
 	}
 	if (ret < 0)
 		return (-1);
-	if (ret == 0 && !s[fd])
+	if (ret == 0 && (str[fd][0] == '\0'))
 		return (0);
-	*line = new_str(s,fd);
-	return (1);
+	return (new_str(str, fd, line));
 }
-//
-//int		main(void)
-//{
-//	int		fd;
-////	char	*str[256];
-//	char	*str2[256];
-//
-//	fd = open("gleb", O_RDONLY);
-//
-//
-//
-//
-//	//fd = open("alp", O_RDONLY);
-//	get_next_line(fd, str2);
-////	get_next_line(fd, str2);
-//	//fd = open("gleb", O_RDONLY);
-//	//printf("%d\n",get_next_line(fd, str));
-//	//printf("%d",get_next_line(fd, str2));
-//	printf("%d",get_next_line(fd, str2));
-//	printf("%s", *str2);
-//	return (0);
-//}
